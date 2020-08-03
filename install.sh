6b96dbd188cb9fa7087832475c5710264dc0f343
Patrick Lubach <racoon63@gmx.net>
2020-08-03 14:28:25 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    curl -LO $HOME/.config/Code/user/keybindings.json https://raw.githubusercontent.com/racoon63/vscode/master/linux/keybindings.json
-    curl -LO $HOME/.config/Code/user/settings.json https://raw.githubusercontent.com/racoon63/vscode/master/linux/settings.json
+    curl -L -s -o $HOME/.config/Code/User/keybindings.json https://raw.githubusercontent.com/racoon63/vscode/master/linux/keybindings.json
+    curl -L -s -o $HOME/.config/Code/User/settings.json https://raw.githubusercontent.com/racoon63/vscode/master/linux/settings.json
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    curl -LO $HOME/.config/Code/user/keybindings.json https://raw.githubusercontent.com/racoon63/vscode/master/mac/keybindings.json
-    curl -LO $HOME/.config/Code/user/settings.json https://raw.githubusercontent.com/racoon63/vscode/master/mac/settings.json
+    curl -L -s -o $HOME/.config/Code/User/keybindings.json https://raw.githubusercontent.com/racoon63/vscode/master/mac/keybindings.json
+    curl -L -s -o $HOME/.config/Code/User/settings.json https://raw.githubusercontent.com/racoon63/vscode/master/mac/settings.json
 fi
