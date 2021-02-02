193e74a8076540c8758e73d3917da0f59d109600
Patrick Lubach <patrick.lubach@wabion.com>
2021-02-02 17:12:44 +0100

    added few plugins every dev should have installed

--- install.sh
+++ install.sh
@@ -10,24 +10,29 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
 fi
 
 echo "=> Install extensions"
+
+code --install-extension ChakrounAnas.turbo-console-log
+code --install-extension CoenraadS.bracket-pair-colorizer
+code --install-extension DavidAnson.vscode-markdownlint
 code --install-extension deerawan.vscode-faker
 code --install-extension eamodio.gitlens
+code --install-extension emilast.LogFileHighlighter
 code --install-extension esbenp.prettier-vscode
 code --install-extension golang.go
 code --install-extension Gruntfuggly.todo-tree
 code --install-extension hashicorp.terraform
 code --install-extension johnpapa.vscode-peacock
+code --install-extension mechatroner.rainbow-csv
+code --install-extension mohsen1.prettify-json
+code --install-extension monokai.theme-monokai-pro-vscode
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
 code --install-extension ms-python.python
+code --install-extension ms-toolsai.jupyter
 code --install-extension ms-vscode-remote.remote-containers
 code --install-extension redhat.vscode-yaml
 code --install-extension softwaredotcom.music-time
 code --install-extension softwaredotcom.swdc-vscode
-code --install-extension mechatroner.rainbow-csv
-code --install-extension monokai.theme-monokai-pro-vscode
 code --install-extension vscode-icons-team.vscode-icons
-code --install-extension vscoss.vscode-ansible
-code --install-extension davidanson.vscode-markdownlint
 
 echo "=> Please restart Visual Studio Code to make sure all extensions are enabled!"
